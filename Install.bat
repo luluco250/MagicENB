06d2aaeec6126aa902b362512e77f6b6b3b1b510
Lucas Melo <luluco250@gmail.com>
2017-06-01 22:34:33 -0300

    Did a bunch of tweaks to the installation script

--- Install.bat
+++ Install.bat
@@ -1,12 +1,42 @@
 @echo off
 SetLocal EnableDelayedExpansion
 
+if not [%1]==[] (
+	set gamedir=%1
+	goto:make_copies
+)
+
 cls
 
+echo.
+echo Welcome to the Magic ENB installation script^^!
+echo.
+echo You can also run the installation directly passing the folder location, like 
+echo 'install "C:\Program Files\Steam\steamapps\common\Skyrim Special Edition"'
+echo.
+echo THIS WILL REPLACE ANY PREVIOUS PRESET PRESENT IN THE GAME FOLDER
+echo.
+:input_gamedir
 echo Please type the game directory location, such as:
 echo "C:\Program Files\Steam\steamapps\common\Skyrim Special Edition"
+echo Without quotes, please. (the "")
+echo Or if you'd like to quit, just press enter without typing anything.
 echo.
 set /p gamedir=Game directory: 
+echo.
+
+if ["%gamedir%"]==[""] (
+	echo Okay, quitting...
+	goto:eof
+)
+
+echo Selected directory: "%gamedir%"
+set /p confirm=Are you sure you want to proceed with the installation? (y/n): 
+if /i not ["%confirm%"]==["y"] (
+	echo Okay, quitting...
+	goto:eof
+)
+echo.
 
 ::making links doesn't really work right now
 goto:make_copies
@@ -29,7 +59,7 @@ set /p type=Type the number of the desired choice:
 
 if [%type%]==[1] goto:make_copies
 if [%type%]==[2] goto:make_symlinks
-if [%type%]==[3] cls && goto:eof
+if [%type%]==[3] goto:eof
 ::else
 echo.
 echo Invalid choice, please type 1 or 2 for the wanted choice!
@@ -40,7 +70,7 @@ goto:choice
 :make_copies
 robocopy "enb" "%gamedir%" /copy:dat /e >nul
 echo Installation successful^^!
-goto:finish
+goto:eof
 
 :make_symlinks
 set origindir=%cd%\ENB
@@ -64,11 +94,3 @@ if not [%errorlevel%]==[0] (
 ) else (
 	echo Installation successful^^!
 )
-
-goto:finish
-
-
-:finish
-pause
-cls
-goto:eof
